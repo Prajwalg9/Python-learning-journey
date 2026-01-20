f6ae30770c4a7d6b50a2f6526eb6daabc4924442
prajwalg9 <prajwalGG99@gmail.com>
2026-01-20 15:01:39 +0530

    selinium

--- 12_OpenCV/03_Resizing_image.py
+++ 12_OpenCV/03_Resizing_image.py
@@ -6,6 +6,9 @@ image_path = 'images/forest_image.png'
 # Load the image using OpenCV
 image = cv2.imread(image_path)
 
+# To check the dimensions of the image
+print("Dimensions of image: ",image.shape)
+
 # Resize the image to a new width and height
 new_width = 800
 new_height = 600
@@ -15,7 +18,7 @@ resized_image = cv2.resize(image, (new_width, new_height))
 cv2.imshow('Resized Forest Image', resized_image)
 
 # Wait for a key press and close the displayed window
-cv2.waitKey(3000)  # Wait for 3000 milliseconds (3 seconds)
+cv2.waitKey(0)  # Wait for 3000 milliseconds (3 seconds)
 
 # Destroy all OpenCV windows
 cv2.destroyAllWindows()
